38861a9cd053d8f459b33e6f73fbb5496fbeebdb
Hamda345 <hdallel13@gmail.com>
2023-05-18 12:12:22 +0100

    zedet icons

--- listeEnseign.php
+++ listeEnseign.php
@@ -78,7 +78,7 @@
             <tbody>
 <?php
     foreach ($liste_enseignant as $item) {
-        echo"<tbody><tr><td>".$item['id_enseignant']."</td><td>".$item['nom_enseignant']."</td><td>".$item['prenom_enseignant']."</td><td>".$item['ecole']."</td><td><a href='supprimerEns.php?dd=$item[0]'>Supprimer</a></td></tr></tbody>";
+        echo"<tbody><tr><td>".$item['id_enseignant']."</td><td>".$item['nom_enseignant']."</td><td>".$item['prenom_enseignant']."</td><td>".$item['ecole']."</td><td><a href='supprimerEns.php?dd=$item[0]' style='color: red;'> <svg width='25' height='25' viewBox='0 0 16 16' fill='currentColor' xmlns='http://www.w3.org/2000/svg'><path d='M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z'/><path d='M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z'/></svg> </a></td></tr></tbody>";
     }
 ?>
             </tbody>
